b75d9b7526516d03c3b30782fbfb089c619e6a0f
Kazuki Imamura <kaizumaki@gmail.com>
2021-07-29 21:27:25 +0900

    fix import path

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 
-import AvatarEditor from 'components//AvatarEditor';
+import AvatarEditor from 'components/AvatarEditor';
 import Colors from 'constants/Colors';
 
 const useStyles = makeStyles((theme) => ({

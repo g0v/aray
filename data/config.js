6eb7847a1a3916671662f2011eec3e1cb89210cd
John Huang <little78926@gmail.com>
2025-02-18 20:37:00 -0800

    update script

--- data/config.js
+++ data/config.js
@@ -4,6 +4,7 @@ module.exports = {
   AWS_PROFILE: AWS_BRANCH ? 'default' : 'jothon',
   AWS_REGION: 'ap-northeast-1',
   APP_SYNC_ID: {
+    ANNIE: 'croehb3fujf6pemzxx5dclodoa',
     DEV: 'w5utc7ntdvaf5d7ej3l432vzgu',
     TST: 'fjsjnmojv5g55nfx7vpala5avu',
     STG: 'jy3bw2qxxbff3me5nqugno7naq',
